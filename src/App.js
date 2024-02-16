4cf8e9a41d691b459ed0144cb1a06cc47112eed4
raynoldyu <rj2yu@uwaterloo.ca>
2024-02-15 21:57:51 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Provider} from 'react-redux';
+import store from './store';
+import RestaurantScreen from './components/RestaurantScreen';
+
+ export default function App() {
+  return (
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
+  );
+}
