d8efc74fef90e51ffc181e5abe7c7671645c2dae
Raynold Yu <83055110+raynoldyu@users.noreply.github.com>
2024-02-15 22:02:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
- export default function App() {
+export default function App() {
   return (
     <Provider store={store}>
       <RestaurantScreen />
